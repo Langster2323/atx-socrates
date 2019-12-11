f4ad1bbe105059f25ac794473283cf48d8877dcd
Langster2323 <langahkeem@gmail.com>
2019-12-11 08:33:05 -0600

    angular practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from './models/user.model';
+import { UserService } from './service/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-ang7';
+export class AppComponent implements OnInit {
+  users$: User[];
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    return this.userService.getUsers().subscribe(user => this.users$ = user);
+  }
 }
